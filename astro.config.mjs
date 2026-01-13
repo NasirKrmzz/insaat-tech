f7c6fc8a9f7d039977a05e61ebba93490e7c3198
nasirkrmzz <nadolin.cs42@gmail.com>
2026-01-13 06:53:22 +0300

    feat: optimize performance & add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bursa-insaat-tadilat.com', // TODO: Domain aldığınızda güncelleyin
+  // TODO: Domain aldığınızda güncelleyin
+  site: 'https://bursa-insaat-tadilat.com',
+
   integrations: [react(), tailwind(), sitemap()],
+  adapter: vercel(),
 });
